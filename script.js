fd66b665cec6fff79560cc218d7cf9ac58099f46
AFZ-NL <ehab@docp.nl>
2024-11-11 21:26:36 +0100

    Add CryptPad viewer files

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿<!DOCTYPE html>
+<html>
+  <head><meta charset="utf-8"></head>
+  <body><p>// Simple script to confirm the page loaded correctly<br>document.addEventListener('DOMContentLoaded', function() {<br>&nbsp; &nbsp; console.log("CryptPad Viewer page loaded successfully.");<br>});<br></p>  </body>
+</html>
